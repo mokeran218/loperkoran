e59652d2f5c5aaf472e7552152f5c953ab85dcfe
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-01 03:37:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,12 +5,6 @@ wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
-
-
-wget -q https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.3.1.10/qbittorrent-nox_linux_x64_static.zip
-unzip -q qbittorrent-nox_linux_x64_static.zip
-export PATH=$PWD:$PATH
-
 mkdir -p downloads
 
 echo "Rclone config detected"
