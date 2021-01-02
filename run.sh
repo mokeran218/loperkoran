6001e007d083f9bf14dc5cbe84862c4f6528fee0
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 03:41:50 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #chmod +x /app/qbittorrent-nox
 #chmod +x /app/ngrok
+chmod +x /app/rclone-v1.51.0-linux-amd64/rclone
 #/app/qbittorrent-nox -d --webui-port=8080
 #/app/ngrok authtoken 1mWgRNV3q4PPA0cHULW4KMZ60o8_32oGEZvoNAgBvPRtgjxVQ
 #/app/ngrok http 8080
@@ -9,4 +10,14 @@ cd tcloud
 
 node server.js
 
+DIR="/app/tcloud/files/"
+while true; do
+    if [ -z "$(ls -A $DIR)" ]; then
+        echo "Empty"
+    else
+        /app/rclone-v1.53.3-linux-amd64/rclone --config /app/rclone-v1.53.3-linux-amd64/rclone.conf move /app/tcloud/files/  Nintoma-muker:hero -vvv --delete-empty-src-dirs --fast-list --tpslimit=5 --transfers=5 --checkers=10 --log-file=/app/rclone.log
+    fi
+
+  sleep 5;
+done
 
