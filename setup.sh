9e97c62fe957674e47388fce12bb8abb24d4143a
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 02:47:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-wget -q https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.3.1.10/qbittorrent-nox_linux_x64_static.zip
-unzip -q qbittorrent-nox_linux_x64_static.zip
-export PATH=$PWD:$PATH
-
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -q ngrok-stable-linux-amd64.zip
 
@@ -13,5 +9,16 @@ unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
 
+echo "===== Download Tcloud App ====="
+URL=$(curl -s https://api.github.com/repos/lunik/tcloud/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+wget $URL -O tcloud.tgz
+
+echo "===== Unarchive App ====="
+mkdir tcloud
+tar zxf tcloud.tgz -C tcloud --strip-components 1
+
+echo "===== Install dependencies ====="
+cd tcloud
+npm install --only=prod
 
 
