677c8f94667f89289f52cc91d401038d4bd275b3
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 03:24:13 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 chmod +x /app/qbittorrent-nox
 /app/qbittorrent-nox -d --webui-port=8080
+/app/ngrok authtoken 1mWgRNV3q4PPA0cHULW4KMZ60o8_32oGEZvoNAgBvPRtgjxVQ
+/app/ngrok http 8080
 
 cd tcloud
 
