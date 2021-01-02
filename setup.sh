96a524ff2fc6d42769ceaf4587273c1b757d2f22
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 02:43:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,14 @@ wget -q https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/downloa
 unzip -q qbittorrent-nox_linux_x64_static.zip
 export PATH=$PWD:$PATH
 
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -q ngrok-stable-linux-amd64.zip
 
 # Install rclone static binary
 wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
-mkdir -p downloads
 
-echo "Rclone config detected"
-echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
 
 
