6b3506709714d17d359951ee6b84afe7daefa931
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 19:25:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ unzip -q ngrok-stable-linux-amd64.zip
 # Install rclone static binary
 wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip rclone-v1.51.0-linux-amd64.zip -d /app/
-export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
 
 echo "===== Unarchive App ====="
