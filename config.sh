b07f1b31b3e95e80a001ac330eca51e47280fb82
mokeran218 <76896074+mokeran218@users.noreply.github.com>
2021-01-04 00:27:54 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,3 @@
-cat << EOF > rclone.conf
-[Nintoma-muker]
-type = drive
-client_id = $MASTER_KEY
-client_secret = BIBYzbrNkAGOexMg_uEPo3lm
-scope = drive
-token = {"access_token":"ya29.a0AfH6SMAo4xH01RIodl4TZN2EdLL7LNwZRH_wrgmhCsFXxWeAk3a4LldBQ1FuvRMq1ndUonBelRWgh9SIFthDJYZSczQOBrERt8cWaKdCUFKcGLrdXlPi1xSrBRjPrOOrM9wQhm6S3E8qOW-LMSIgkKXLi3x2kfUb4POWWpdcQZ815w","token_type":"Bearer","refresh_token":"1//0g6ti6BtuDU3gCgYIARAAGBASNwF-L9IrOe8r_fg87-CsjGkYGw-ILqpDvQFt-8tF_zZxouE1A7iruwVw9VJagBWAJ3esT9zMIuE","expiry":"2021-01-02T21:29:52.858117294+07:00"}
-team_drive = 0AJMfNQ4TuRMiUk9PVA
+cat << EOF > muk.json
+$MASTER_KEY
 EOF
