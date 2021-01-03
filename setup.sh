f1a739b33ebd57d51fab1f17ae5759a8d088d5bc
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 23:03:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,16 +7,16 @@ unzip -q ngrok-stable-linux-amd64.zip
 wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
 unzip -q rclone-v1.51.0-linux-amd64.zip 
 
-echo "===== Download Tcloud App ====="
-URL=$(curl -s https://api.github.com/repos/lunik/tcloud/releases/latest | grep browser_download_url | cut -d '"' -f 4)
-wget $URL -O tcloud.tgz
+# echo "===== Download Tcloud App ====="
+# URL=$(curl -s https://api.github.com/repos/lunik/tcloud/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+# wget $URL -O tcloud.tgz
 
-echo "===== Unarchive App ====="
-mkdir tcloud
-tar zxf tcloud.tgz -C tcloud --strip-components 1
-rm tcloud/public/index.html
-mv index.html tcloud/public/index.html
+# echo "===== Unarchive App ====="
+# mkdir tcloud
+# tar zxf tcloud.tgz -C tcloud --strip-components 1
+# rm tcloud/public/index.html
+# mv index.html tcloud/public/index.html
 
-echo "===== Install dependencies ====="
-cd tcloud
-npm install --only=prod
+# echo "===== Install dependencies ====="
+# cd tcloud
+# npm install --only=prod
