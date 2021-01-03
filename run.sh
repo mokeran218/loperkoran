0375a61c168306aca68950ef1d93a035ddfdd535
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-03 20:01:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ chmod +x /app/rclone
 nohup /app/qbittorrent-nox -d --webui-port=8080 &
 nohup /app/ngrok authtoken 1mYe6zSkY2smRhZJMiZXew8I5RD_46CfS6QhJiTGdy2uJNo57 &
 chmod +x /app/ngok.sh
-
+chmod +x /app/config.sh
+/app/config.sh
 
 cd tcloud
 
